a5f01a21dec5a2a3caad3fbcf26230d0658e2694
Jhay Oteda <jhayoteda9gmail.com>
2026-05-10 23:09:06 +0800

    fixed server port

--- backend/server.ts
+++ backend/server.ts
@@ -24,6 +24,7 @@ app.use('/api-docs', swaggerDocs);
 // global error handler
 app.use(errorHandler);
 
+
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log('Server listening on port ' + port));
