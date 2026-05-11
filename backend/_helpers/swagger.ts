4c5adc0fa436cc650894d79d7f52ffda4b584b0b
Jhay Oteda <jhayoteda9gmail.com>
2026-05-12 00:14:18 +0800

    restored swagger cdn for visual fix

--- backend/_helpers/swagger.ts
+++ backend/_helpers/swagger.ts
@@ -6,6 +6,14 @@ import YAML from 'yamljs';
 import path from 'path';
 const swaggerDocument = YAML.load(path.join(process.cwd(), 'backend/swagger.yaml'));
 
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const options = {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js'
+    ]
+};
+
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 export default router;
